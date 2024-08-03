5687ee4a7ff264399186f1c1ace6144728954858
chan9yu <wry5533@gmail.com>
2024-08-03 17:03:07 +0900

    chore: vanilla-extract plugin 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), vanillaExtractPlugin()],
 	server: {
-		port: 3030,
-	},
+		port: 3030
+	}
 });
