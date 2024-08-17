c107e97aea28735339cea78d73f85c6418446404
chan9yu <wry5533@gmail.com>
2024-08-17 17:09:07 +0900

    feat: TabMenu 컴포넌트 구성 및 폴더 정리

---
+++ src/components/layout/_private/TabMenu/TabMenu.tsx
@@ -0,0 +1,30 @@
+import { ComponentProps } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import type { AppRoutesType } from '../../../../constants/router';
+import { Icon, Text } from '../../../common';
+import * as styles from './TabMenu.css';
+
+type TabMenuProps = {
+	iconName: ComponentProps<typeof Icon>['name'];
+	isActive: boolean;
+	label: string;
+	to: AppRoutesType;
+};
+
+export default function TabMenu({ iconName, isActive, label, to }: TabMenuProps) {
+	const navigate = useNavigate();
+
+	const handleNavigation = () => {
+		!isActive && navigate(to);
+	};
+
+	return (
+		<li className={styles.container} onClick={handleNavigation}>
+			<Icon name={iconName} color={isActive ? 'primary.B300' : 'grayscale.G300'} className={styles.transition} />
+			<Text size="xs" color={isActive ? 'primary.B300' : 'grayscale.G300'} className={styles.transition}>
+				{label}
+			</Text>
+		</li>
+	);
+}
