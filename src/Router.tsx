85cea3313cb7c71f2ff14608ee66bb5a21cccb11
khmin <khmin@rsupport.com>
2024-08-29 11:51:30 +0900

    feat: 로그인 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter, type RouteObject } from 'react-router-dom';
+import { Navigate, RouterProvider, createBrowserRouter, type RouteObject } from 'react-router-dom';
 
 import { PostLoginLayout, PreLoginLayout } from './components/layout';
 import { APP_ROUTES } from './constants/router';
@@ -7,13 +7,14 @@ import * as P from './pages';
 /** 로그인 전 라우터 구성 */
 const preLoginRouter: RouteObject = {
 	element: <PreLoginLayout />,
-	children: [{ path: APP_ROUTES.ROOT, element: <P.Login /> }]
+	children: [{ path: APP_ROUTES.LOGIN, element: <P.Login /> }]
 };
 
 /** 로그인 후 라우터 구성 */
 const postLoginRouter: RouteObject = {
 	element: <PostLoginLayout />,
 	children: [
+		{ index: true, element: <Navigate to={APP_ROUTES.SHUTTLE_BUS} replace /> },
 		{ path: APP_ROUTES.SHUTTLE_BUS, element: <P.ShuttleBus /> },
 		{ path: APP_ROUTES.VILLAGE_BUS, element: <P.VillageBus /> },
 		{ path: APP_ROUTES.COMMUNITY, element: <P.Community /> }
--- src/components/layout/PreLoginLayout/PreLoginLayout.tsx
+++ src/components/layout/PreLoginLayout/PreLoginLayout.tsx
@@ -1,19 +1,8 @@
 import { Outlet, useNavigate } from 'react-router-dom';
-import { APP_ROUTES } from '../../../constants/router';
 
 export default function PreLoginLayout() {
-	const navigate = useNavigate();
-
-	const handleLoginTest = () => {
-		navigate(APP_ROUTES.SHUTTLE_BUS, { replace: true });
-	};
-
 	return (
 		<div>
-			<h2>PreLoginLayout</h2>
-			<br />
-			<div onClick={handleLoginTest}>Login</div>
-			<br />
 			<Outlet />
 		</div>
 	);
