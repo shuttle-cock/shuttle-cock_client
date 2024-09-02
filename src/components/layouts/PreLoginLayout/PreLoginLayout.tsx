a1b0e753b4fc648879ac89c9ef127182150ed244
khmin <khmin@rsupport.com>
2024-09-02 16:38:47 +0900

    fix: userInfo 호출 변경

--- src/components/layouts/PreLoginLayout/PreLoginLayout.tsx
+++ src/components/layouts/PreLoginLayout/PreLoginLayout.tsx
@@ -4,10 +4,10 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import { APP_ROUTES } from '../../../constants/router';
 import { loadUserInfo } from '../../../services/storageService';
 
-const { userId } = loadUserInfo();
 
 export default function PreLoginLayout() {
 	const navigate = useNavigate();
+	const { userId } = loadUserInfo();
 
 	useEffect(() => {
 		if (!!userId) {
