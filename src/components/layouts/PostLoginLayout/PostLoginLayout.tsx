a8984d6d5ef0836cc186b1e031e64cae799bbb04
khmin <khmin@rsupport.com>
2024-10-07 11:48:05 +0900

    fix(#0): 권한체크 주석 제거

--- src/components/layouts/PostLoginLayout/PostLoginLayout.tsx
+++ src/components/layouts/PostLoginLayout/PostLoginLayout.tsx
@@ -4,15 +4,16 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import { loadUserInfo } from '@services/storageService';
 import { Header, TabMenus } from '../_private';
 import * as styles from './PostLoginLayout.css';
+import { APP_ROUTES } from '@constants/routerConstants';
 
 export default function PostLoginLayout() {
 	const navigate = useNavigate();
 	const { userId } = loadUserInfo();
 
 	useEffect(() => {
-		// if (!userId) {
-		// 	navigate(APP_ROUTES.LOGIN, { replace: true });
-		// }
+		if (!userId) {
+			navigate(APP_ROUTES.LOGIN, { replace: true });
+		}
 	}, []);
 
 	return (
